f1c7066c4f34e9bceeae2f3d7e3232aaf98f86bb
Feras Aljoudi <feras.aljoudi@gmail.com>
2024-11-28 11:40:09 -0600

    Adding to setup.sh

--- Code/RaspberryPi5/setup.sh
+++ Code/RaspberryPi5/setup.sh
@@ -21,3 +21,20 @@ sudo apt autoclean
 
 # Rebooting the system
 sudo reboot
+
+# Testing the speaker
+speaker-test -D hw:0,0 -t sine -f 440 -c 2 -l 1
+
+# Increasing the volume
+alsamixer
+
+# Press F6 to select a sound card.
+# Use the arrow keys to navigate to your USB speaker and press Enter.
+# Use the Up Arrow key to increase the volume.
+
+# Save the setting
+sudo alsactl store
+
+# Testing the speaker again
+speaker-test -D hw:0,0 -t sine -f 440 -c 2 -l 1
+
