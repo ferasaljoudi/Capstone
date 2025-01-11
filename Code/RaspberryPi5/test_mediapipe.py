6f508e5431d54ac9a748563a854f765abe684064
Feras Aljoudi <feras.aljoudi@gmail.com>
2025-01-11 10:14:38 -0600

    Update Tests

--- Code/RaspberryPi5/test_mediapipe.py
+++ Code/RaspberryPi5/test_mediapipe.py
@@ -1,7 +1,8 @@
+import time
 import unittest
 
-from eye_detection_mediapipe import calculate_eye_ratio, calculate_mouth_ratio
-
+from eye_detection_mediapipe import (calculate_eye_ratio,
+                                    calculate_mouth_ratio, detect_yawn)
 
 class TestDrowsinessDetection(unittest.TestCase):
     def test_valid_eye_ratio(self):
@@ -18,7 +19,7 @@ class TestDrowsinessDetection(unittest.TestCase):
         self.assertAlmostEqual(result, expected_ratio, places=2)
 
     def test_nonvalid_eye_ratio(self):
-        # Simulate invalid eye landmarks (missing points)
+        # Simulate invalid eye landmarks (missing point)
         eye_coords = [
             (30, 60),  # Top point
             (30, 20),  # Bottom point
@@ -38,6 +39,58 @@ class TestDrowsinessDetection(unittest.TestCase):
         expected_ratio = (30 / 40) * 100
         result = calculate_mouth_ratio(upper_lip, lower_lip, left_lip, right_lip)
         self.assertAlmostEqual(result, expected_ratio, places=2)
+
+    def test_no_yawn_detected_below_boundary1(self):
+        # Assume yawn started less than 2 seconds ago
+        yawn_start_time = time.time() - 1.9
+        # At threshold
+        average_ratio_lips = 30
+        # Default variables
+        yawn_detected, yawn_alert = False, False
+        new_yawn_start_time, new_yawn_detected, new_yawn_alert = detect_yawn(
+            average_ratio_lips, yawn_start_time, yawn_detected, yawn_alert, threshold=30, duration=2)
+        # Yawn should not be detected
+        self.assertFalse(new_yawn_detected)
+        self.assertFalse(new_yawn_alert)
+
+    def test_no_yawn_detected_below_boundary2(self):
+        # Assume yawn started 2 seconds ago
+        yawn_start_time = time.time() - 2
+        # Below threshold
+        average_ratio_lips = 29.9
+        # Default variables
+        yawn_detected, yawn_alert = False, False
+        new_yawn_start_time, new_yawn_detected, new_yawn_alert = detect_yawn(
+            average_ratio_lips, yawn_start_time, yawn_detected, yawn_alert, threshold=30, duration=2)
+        # Yawn should not be detected
+        self.assertFalse(new_yawn_detected)
+        self.assertFalse(new_yawn_alert)
+
+    def test_yawn_detected_at_boundary(self):
+        # Assume yawn started 2 seconds ago
+        yawn_start_time = time.time() - 2
+        # At threshold
+        average_ratio_lips = 30
+        # Default variables
+        yawn_detected, yawn_alert = False, False
+        new_yawn_start_time, new_yawn_detected, new_yawn_alert = detect_yawn(
+            average_ratio_lips, yawn_start_time, yawn_detected, yawn_alert, threshold=30, duration=2)
+        # Yawn should be detected
+        self.assertTrue(new_yawn_detected)
+        self.assertTrue(new_yawn_alert)
+
+    def test_yawn_detected_above_boundary(self):
+        # Assume yawn started 2.1 seconds ago
+        yawn_start_time = time.time() - 2.1
+        # Above threshold
+        average_ratio_lips = 31
+        # Default variables
+        yawn_detected, yawn_alert = False, False
+        new_yawn_start_time, new_yawn_detected, new_yawn_alert = detect_yawn(
+            average_ratio_lips, yawn_start_time, yawn_detected, yawn_alert, threshold=30, duration=2)
+        # Yawn should be detected
+        self.assertTrue(new_yawn_detected)
+        self.assertTrue(new_yawn_alert)
 
 if __name__ == "__main__":
     unittest.main()
